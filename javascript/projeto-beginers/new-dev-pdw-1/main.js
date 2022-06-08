9057e19fe21e3f6045a65bb9201ac15cc12c8ee6
Lucas Gabriel Antoniolli <lucasgabrielgremio1.com>
2022-06-07 21:28:21 -0300

    07/06

--- javascript/projeto-beginers/new-dev-pdw-1/main.js
+++ javascript/projeto-beginers/new-dev-pdw-1/main.js
@@ -1,21 +1,84 @@
-const addbutton = document.getElementById('addbutton');
+let peoples = [];
 
-const buttonAdd = function()  {
-  event.preventDefault();
+const iconEdit = document.createElement('i');
+iconEdit.setAttribute('title', 'Editar');
+iconEdit.setAttribute('class', 'fa-solid fa-pencil');
+iconEdit.setAttribute('style', 'cursor: pointer; margin-inline: 0.5rem');
 
-  const message = {
-     name: document.getElementById('name').value,
-     tell: document.getElementById('tell').value,
-     years: document.getElementById('years').value,
-     adress: document.getElementById('adress').value,
-     CPF: document.getElementById('CPF').value
-  }
 
-  myStorage = localStorage;
+const iconRemove = document.createElement('i');
+iconRemove.setAttribute('title', 'Remover');
+iconRemove.setAttribute('class', 'fa-solid fa-trash');
+iconRemove.setAttribute('style', 'cursor: pointer; margin-inline: 0.5rem');
+
+const EditPeople = () => {
+console.log('edit')
+}
+
+const RemovePeople = () => {
+    console.log('remove')
+}
+
+const loadPeoples = () => {
+  const itemsJaArmazenados = localStorage.getItem('listaDePessoas');
+  return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : [];
+}
+
+const listPeoples = () => {
+  const peoples = loadPeoples();
   
-  console.log(message);
- document.querySelector('form').reset();
- }
+  let ul = document.querySelector('ul');
+  if (ul) {
+    ul.remove();
+  }
+  
+  ul = document.createElement('ul');
+ 
+  peoples.forEach((item, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `Nome: ${item.name} Idade: ${item.age}, Altura: ${item.height}`
+   
+    li.appendChild(span());
+   
+    ul.appendChild(li);
+    
+  });
+  
+  document.getElementById('list-section').appendChild(ul);
+}
 
-addbutton.addEventListener('click', buttonAdd);
+const span = () => {
+    const span =document.createElement('span');
+    span.appendChild(iconRemove);
+    span.appendChild(iconEdit);
+    
+    return span;
+}
 
+listPeoples();
+
+const addPeople = (event) => {
+  event.preventDefault();
+  const people = {
+    name: document.getElementById('name').value,
+    age: document.getElementById('age').value,
+    height: document.getElementById('height').value,
+  }
+  
+  peoples = loadPeoples();
+
+  peoples.push(people);
+  
+  localStorage.setItem('listaDePessoas', JSON.stringify(peoples));
+
+  document.querySelector('form').reset();
+
+  listPeoples();
+}
+
+iconEdit.addEventListener('click', EditPeople);
+
+iconRemove.addEventListener('click', RemovePeople);
+
+const botaoDeAdicionar = document.getElementById('btn');
+botaoDeAdicionar.addEventListener('click', addPeople)
