2c8070e40e291f5d80d554ebbb520d4ae775e68d
Lucas Gabriel Antoniolli <lucasgabrielgremio1.com>
2022-06-14 21:28:56 -0300

    14/06

--- segunda-etapa/controle-estoque-web/main.js
+++ segunda-etapa/controle-estoque-web/main.js
@@ -16,8 +16,10 @@ const addRegistration = (event) => {
     arrayRegistration[counterArray] = registration;
     storage.setItem('storageRegistration', JSON.stringify(arrayRegistration)); 
 
+    console.log(arrayRegistration);
+
     counterArray++
-  };
+};
 
   const listRegistration = (event) => {
     event.preventDefault();
@@ -26,30 +28,51 @@ const addRegistration = (event) => {
     console.log(arrayStorage);
 
     let ul = document.createElement('ul');
-    let identificador = 0;
+    let identifi = 0;
 
-    arrayStorage.forEach((item, identificador) => {
+    arrayStorage.forEach((item, identifi) => {
       let li = document.createElement('li');
      
       li.innerHTML = 
-      `
-      Marca: ${JSON.stringify(arrayStorage[identificador].brand)} 
-      Modelo: ${JSON.stringify(arrayStorage[identificador].model)} 
-      Ano: ${JSON.stringify(arrayStorage[identificador].year)} 
-      Quantidade: ${JSON.stringify(arrayStorage[identificador].units)} 
-      Código: ${JSON.stringify(arrayStorage[identificador].counter)}
+      `Marca: ${JSON.stringify(arrayStorage[identifi].brand)}
+       Modelo: ${JSON.stringify(arrayStorage[identifi].model)} 
+       Ano: ${JSON.stringify(arrayStorage[identifi].year)}
+       Código: ${JSON.stringify(arrayStorage[identifi].counter)}
+       Quantidade: ${JSON.stringify(arrayStorage[identifi].units)}
       `;
 
       ul.appendChild(li);
-      identificador++
+
+        let iconDown = document.createElement('i');
+        iconDown.setAttribute('class', 'fa-solid fa-angle-down');
+        iconDown.setAttribute('id', 'icon-down');
+        iconDown.setAttribute('style', 'cursor: pointer; margin-inline: 0.5rem');
+        iconDown.setAttribute('title', 'Diminuir');
+        iconDown.setAttribute('onclick', `onClickDown(this)`);
+        iconDown.setAttribute('number-id', `${identifi}`);
+
+        let iconUp = document.createElement('i');
+        iconUp.setAttribute('class', 'fa-solid fa-angle-up');
+        iconUp.setAttribute('id', 'icon-Up');
+        iconUp.setAttribute('style', 'cursor: pointer; margin-inline: 0.5rem');
+        iconUp.setAttribute('title', 'Aumentar');
+        iconUp.setAttribute('onclick', `onClickUp(this)`);
+        iconUp.setAttribute('number-id', `${identifi}`);
+
+        li.appendChild(iconDown);
+        li.appendChild(iconUp);   
+
+      identifi++;
     });
     
-    document.getElementById('section-list').appendChild(ul); 
+    document.getElementById('section-list').appendChild(ul)
   };
-
+                                                                                              
 
   const consultRegistration = (event) => {
     event.preventDefault();
+
+    console.log(storage.storageRegistration);
 
     let arrayStorage = JSON.parse(storage.storageRegistration);
     console.log(arrayStorage);
@@ -57,15 +80,33 @@ const addRegistration = (event) => {
     let codeVehicle = document.getElementById('vehicle-code').value;
     console.log('code:', codeVehicle);
 
+    if(!codeVehicle){
+      codeVehicle = 0;
+    }
+
     let ul = document.createElement('ul');
     document.getElementById('section-list').appendChild(ul);
 
     let li = document.createElement('li');
-    li.innerHTML = JSON.stringify(arrayStorage[codeVehicle]);
+    li.innerHTML = `
+      Marca: ${JSON.stringify(arrayStorage[codeVehicle].brand)} 
+      Modelo: ${JSON.stringify(arrayStorage[codeVehicle].model)} 
+      Ano: ${JSON.stringify(arrayStorage[codeVehicle].year)} 
+      Código: ${JSON.stringify(arrayStorage[codeVehicle].counter)}
+      Quantidade: ${JSON.stringify(arrayStorage[codeVehicle].units)}
+      `;
     ul.appendChild(li);
   };
-  
-  
+
+  const onClickUp = (event) => {
+    console.log('Up')
+  };
+
+  const onClickDown = (event) => {
+     console.log('Down')
+  };
+
+
   const buttonConsultRegistration = document.getElementById('consult-vehicle-code');
 
   if(buttonConsultRegistration)
