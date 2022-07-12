2359b73186effc4730a5f0d392781ebc9d53495c
Lucas Gabriel Antoniolli <lucasgabrielgremio1.com>
2022-07-11 21:29:13 -0300

    11-07

--- terceira-etapa/node/api-rest/src/controllers/AuthorController.js
+++ terceira-etapa/node/api-rest/src/controllers/AuthorController.js
@@ -1,13 +1,25 @@
-exports.findAll = (request, response) => {
-  const query = request.query;
-  console.log('query string authors', query);
-  console.log('API is available');
-  return response.status(200).send('Acessando recurso / authors METHOD: GET');
+const database = require('../databases/knex');
+
+exports.findAll = async (request, response) => {
+ 
+  try{
+    const sql = await database.select('*').from('authors');
+  
+    return response.status(200).send({authors: sql});
+
+  } catch (error) {
+    return response.status(500).send({ error: error?.message || e });
+  }
+
 }
 
-exports.create = (request, response) => {
-  console.log('Recebendo dados', request.body);
-  return response.status(200).send('Acessando recurso / authors METHOD: POST');
+exports.create = async (request, response) => {
+  try {
+    await database('authors').insert(request.body);
+    return response.status(200).send({status: 'success'});
+  } catch (error) {
+    return response.status(500).send({ error: error?.message || e });
+  }
 }
 
 exports.getById = (request, response) => {
---
+++ terceira-etapa/node/api-rest/src/databases/knex.js
@@ -0,0 +1,7 @@
+const environment = process.env.ENVIRONMENT || 'development';
+
+const config = require('../../knexfile')[environment];
+
+const knex = require('knex')(config);
+
+module.exports = knex;
